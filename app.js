6cb02c5b481266512bae563e59313fecb146aba4
SRS <robbertsergiu@gmail.com>
2020-11-07 23:36:34 +0200

    ll

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 
-const jokes  = document.querySelector("#jokes");
-const getJoke = async() => {
-  const config = { headers: { Accept: 'application/json'}}
-  const res = await axios.get('https://icanhazdadjoke.com/', config);
+const newJoke = async () => {
+  const getNewJoke =  await getJoke();
   const newLi = document.createElement('li')
-  newLi.append(res.data.joke);
+  newLi.append(getNewJoke);
   jokes.append(newLi);
 }
+
+const getJoke = async() => {
+  try{
+    const config = { headers: { Accept: 'application/json'}}
+  const res = await axios.get('https://icanhazdadjoke.com/', config);
+ return res.data.joke;
+  }catch(e){
+    return "No Jokes available!"
+  }
+}
+
 const btn  = document.querySelector("button");
-btn.addEventListener('click', getJoke)
+btn.addEventListener('click', newJoke)
+const jokes  = document.querySelector("#jokes");
